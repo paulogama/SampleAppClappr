7f553a2818844c1334ae526421824f1ae5ec5eee
Paulo Gama <pgama@thoughtworks.com>
2017-07-18 16:14:14 -0300

    Initial Commit

---
+++ SampleAppClappr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleAppClappr
+//
+//  Created by Paulo Gama on 18/07/17.
+//  Copyright © 2017 Paulo Gama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
